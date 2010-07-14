47ac5ba6f512ab71cc17422a4270c8bcd20c29eb
Andrew O'Brien <andrew@econify.com>
2010-07-13 21:23:08 -0400

    Added test

--- Rakefile
+++ Rakefile
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'rake'
+require 'spec/rake/spectask'
 
+
+desc "Run the specs under spec/"
+Spec::Rake::SpecTask.new do |t|
+  t.spec_opts = ['--colour', "--format", 'profile', '--diff']
+  t.spec_files = FileList['spec/**/*_spec.rb']
+end
+
+desc "Generate ruby files from thrift"
 namespace "thrift" do
   task "gen" do
     sh "thrift --gen rb -o lib/ lib/Hbase.thrift"
---
+++ spec/hbase_thrift_spec.rb
@@ -0,0 +1,21 @@
+$:.unshift File.dirname(__FILE__) + "/../"
+
+require 'lib/hbase_thrift'
+
+require 'rubygems'
+require 'spec'
+
+HBASE_CONNECTION_PARAMS = ['127.0.0.1', 9090]
+
+describe HbaseThrift do
+  it "connects to a running HBase thrift server" do
+    lambda do
+      t = Thrift::BufferedTransport.new(Thrift::Socket.new(*HBASE_CONNECTION_PARAMS))
+      p = Thrift::BinaryProtocol.new(t)
+      c = Apache::Hadoop::Hbase::Thrift::Hbase::Client.new(p)
+      t.open
+      
+      c.getTableNames
+    end.should_not raise_exception
+  end
+end
